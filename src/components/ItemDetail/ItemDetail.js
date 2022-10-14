050ec14c14e3a3d91a6b8dad45892a70311697e4
Leonardo Bello <leomdqarg@gmail.com>
2022-10-13 22:44:12 -0300

    spread operator para item detail

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,16 +1,16 @@
 import ItemCount from "../ItemCount/ItemCount.js"
-const ItemDetail = ({item}) => {
+const ItemDetail = ({id, name, img, category, description, price, stock, setCart}) => {
     return (
         <div className="row gx-4 gx-lg-5 align-items-center">
-            <div className="col-md-6"><img className="card-img-top mb-5 mb-md-0" src={item?.img} alt={item?.name} /></div>
+            <div className="col-md-6"><img className="card-img-top mb-5 mb-md-0" src={img} alt={name} /></div>
             <div className="col-md-6">
-                <div className="small mb-1">SKU: BST-{ item?.id }</div>
-                <h1 className="display-5 fw-bolder">{ item?.name }</h1>
+                <div className="small mb-1">SKU: BST-{ id }</div>
+                <h1 className="display-5 fw-bolder">{ name }</h1>
                 <div className="fs-5 mb-5">
-                    <span>${ item?.price }</span>
+                    <span>${ price }</span>
                 </div>
-                <p className="lead">{ item?.description }</p>
-                <ItemCount initial={2} stock={20} />
+                <p className="lead">{ description }</p>
+                <ItemCount initial={2} stock={stock} />
             </div>
         </div>
     )
--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -37,9 +37,9 @@ const ItemDetailContainer = () => {
     return (
         <section className="py-5">
             <div className="container px-4 px-lg-5 my-5">
-                <ItemDetail item={item} />
+                <ItemDetail {...item} />
             </div>
-    </section>
+        </section>
     );
   }
 
