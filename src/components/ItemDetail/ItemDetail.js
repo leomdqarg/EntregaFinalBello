a611c7022588f47ea0e4e25ef940f47b044f1510
Leonardo Bello <leomdqarg@gmail.com>
2022-10-25 22:18:15 -0300

    ajuste de diseño y agregue helper FormatPrice

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,6 +1,7 @@
 import { useState, useContext } from 'react'
 import { Link } from 'react-router-dom'
 import { CartContext } from '../../context/CartContext'
+import { FormatPrice } from '../../Helpers/FormatPrice'
 
 import ItemCount from "../ItemCount/ItemCount.js"
 
@@ -20,29 +21,35 @@ const ItemDetail = ({id, name, img, category, description, price, stock}) => {
 
         addItem(productToAdd)
     }
-    return (
-        <div className="row gx-4 gx-lg-5 align-items-center">
-            <div className="col-md-6"><img className="card-img-top mb-5 mb-md-0" src={img} alt={name} /></div>
-            <div className="col-md-6">
-                <div className="small mb-1">SKU: BST-{ id }</div>
-                <h1 className="display-5 fw-bolder">{ name }</h1>
-                <div className="fs-5 mb-5">
-                    <span>${ price }</span>
-                </div>
-                <p className="lead">{ description }</p>
-                {
-                    quantityToAdd === 0 ? (
-                        <ItemCount initial={productAddedQuantity} stock={stock} onAdd={handleOnAdd} />
-                   ) : (
-                    <>
-                    <Link to={`/categoria/${category}`} className='btn btn-primary col-12 mb-2'>Seguir Comprando</Link>
-                    <Link to='/cart' className='btn btn-success col-12'>Finalizar compra</Link>
-                    </>
-                    )
-                }
 
+    return (
+        <section className="py-5">
+            <div className="container px-4 px-lg-5 my-5">
+                <div className="row gx-4 gx-lg-5 align-items-center">
+                    <div className="col-md-6">
+                        <img className="card-img-top mb-5 mb-md-0" src={img} alt={name} />
+                    </div>
+                    <div className="col-md-6">
+                        <div className="small mb-1">SKU: BST-{ id }</div>
+                        <h1 className="display-5 fw-bolder">{ name }</h1>
+                        <div className="fs-5 mb-5">
+                            <span>{FormatPrice(price) }</span>
+                        </div>
+                        <p className="lead">{ description }</p>
+                        {
+                            quantityToAdd === 0 ? (
+                                <ItemCount initial={productAddedQuantity} stock={stock} onAdd={handleOnAdd} />
+                            ) : (
+                            <>
+                                <Link to={`/categoria/${category}`} className='btn btn-primary col-12 mb-2'>Seguir Comprando</Link>
+                                <Link to='/cart' className='btn btn-success col-12'>Finalizar compra</Link>
+                            </>
+                            )
+                        }
+                    </div>
+                </div>
             </div>
-        </div>
+        </section>
     )
 }
 
