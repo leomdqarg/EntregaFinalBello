db4855eb42456ee61b949806916612e792f915d5
Leonardo Bello <leomdqarg@gmail.com>
2022-10-29 19:56:07 -0300

    borrado por problemas de may/min

--- src/Helpers/FormatPrice.js
+++
@@ -1,4 +0,0 @@
-export const formatPrice = (price, locale='es-AR') => {
-    let localPrice = new Intl.NumberFormat(locale, {style: 'currency', currency: 'ARS'}).format(price)
-    return localPrice
-}
