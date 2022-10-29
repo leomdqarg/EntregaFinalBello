cafc37cae40e40e41db7a61f6496e1695cd35cb8
Leonardo Bello <leomdqarg@gmail.com>
2022-10-28 21:49:09 -0300

    Cambio en estilo de errores en checkout Traduccion de labels y mensajes de errores

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -111,34 +111,34 @@ const Checkout = () => {
                                     validate={values => {
                                         const errors = {};
                                         if (!values.email) {
-                                            errors.email = 'Required';
+                                            errors.email = 'Debe completar este campo';
                                         } else if (
                                             !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
                                         ) {
-                                            errors.email = 'Invalid email address';
+                                            errors.email = 'Verifique el formato de la direccion de correo ';
                                         }
                                         if (!values.emailRepeated) {
-                                        errors.emailRepeated = 'Required';
+                                        errors.emailRepeated = 'Debe completar este campo';
                                         } else if (
                                         !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.emailRepeated)
                                         ) {
-                                        errors.emailRepeated = 'Invalid email address';
+                                        errors.emailRepeated = 'Verifique el formato de la direccion de correo ';
                                         }
                                         if(!errors.emailRepeated && !errors.email && values.email !== values.emailRepeated) {
-                                            errors.emailRepeated = 'Email and Email Repetead must be the same';
+                                            errors.emailRepeated = 'Las direcciones de correo de electronico no coinciden.';
                                         }
                                         if (!values.firstName) {
-                                            errors.firstName = 'Required'
+                                            errors.firstName = 'Debe completar este campo';
                                         }
                                         if (!values.lastName) {
-                                            errors.lastName = 'Required'
+                                            errors.lastName = 'Debe completar este campo';
                                         }
                                         if (!values.phone) {
-                                            errors.phone = 'Required'
+                                            errors.phone = 'Debe completar este campo';
                                         } else if (
                                             !/^[+]?[(]?[0-9]{3}[)]?[-\s.]?[0-9]{3}[-\s.]?[0-9]{4,6}$/im.test(values.phone)
                                         ) {
-                                            errors.phone = 'Invalid phone number'
+                                            errors.phone = 'Formato de telefono erroneo. Debe ser solo numeros o guines y debe tener maximo 12 digitos. Ej. 123-456-12345 ó 1234567890'
                                         }
 
 
@@ -152,7 +152,7 @@ const Checkout = () => {
                                             emptyCart()
                                         }).catch(error => {
                                             Store.addNotification({
-                                                title: "Error de red",
+                                                title: "Error.",
                                                 message: 'No se pudo realizar el pedido. Intente luego',
                                                 type: "warning",
                                                 insert: "top",
@@ -171,47 +171,43 @@ const Checkout = () => {
                                     }}
                                 >
                                     {({
-                                        values,
                                         errors,
-                                        touched,
-                                        handleChange,
-                                        handleBlur,
-                                        handleSubmit,
                                         isSubmitting,
                                         /* and other goodies */
                                     }) => (
                                         <Form>
                                             <div className="row g-3">
                                                 <div className="col-sm-6">
-                                                    <label htmlFor="email" className="form-label">Email</label>
+                                                    <label htmlFor="email" className="form-label">Correo Electronico</label>
                                                     <Field type="email" name="email" className={`form-control ${!errors.email ? '' : ' is-invalid' } `} placeholder="Email" />
-                                                        <ErrorMessage name="email" className="alert alert-danger" component="p" />
+                                                        <ErrorMessage name="email" className="invalid-feedback" component="div" />
                                                 </div>
                                                 <div className="col-sm-6">
-                                                    <label htmlFor="email" className="form-label">Repeat Email</label>
+                                                    <label htmlFor="email" className="form-label">Repita el Correo Electronico</label>
                                                     <Field type="email" name="emailRepeated" className={`form-control ${!errors.emailRepeated ? '' : ' is-invalid' } `} placeholder="Repeat Email" />
-                                                    <ErrorMessage name="emailRepeated" className="alert alert-danger" component="p" />
+                                                    <ErrorMessage name="emailRepeated" className="invalid-feedback" component="div" />
                                                 </div>
                                             </div>
                                             <div className="row g-12">
                                                 <div className="col">
-                                                    <label htmlFor="firstName" className="form-label">First Name</label>
+                                                    <label htmlFor="firstName" className="form-label">Nombre</label>
                                                     <Field type="text" className={`form-control ${!errors.firstName ? '' : ' is-invalid' } `} name="firstName" placeholder="First Name" />
-                                                    <ErrorMessage name="firstName" className="alert alert-danger" component="p" />
+                                                    <ErrorMessage name="firstName" className="invalid-feedback" component="div" />
                                                 </div>
                                             </div>
                                             <div className="row g-12">
                                                 <div className="col">
-                                                    <label htmlFor="lastName" className="form-label">Last Name</label>
+                                                    <label htmlFor="lastName" className="form-label">Apellido</label>
                                                     <Field type="text" className={`form-control ${!errors.lastName ? '' : ' is-invalid' } `} name="lastName" placeholder="Last Name" />
-                                                    <ErrorMessage className="alert alert-danger" name="lastName" component="p" />
+                                                    <ErrorMessage className="invalid-feedback" name="lastName" component="div" />
                                                 </div>
                                             </div>
                                             <div className="row g-3">
                                                 <div className="col-sm-12">
-                                                    <label htmlFor="email" className="form-label">Phone</label>
+                                                    <label htmlFor="email" className="form-label">Teléfono</label>
                                                     <Field type="text" className={`form-control ${!errors.phone ? '' : ' is-invalid' } `} name="phone" placeholder="Phone Number" />
-                                                    <ErrorMessage className="alert alert-danger"name="phone" component="p" />
+                                                    <ErrorMessage className="invalid-feedback"name="phone" component="div" />
+                                                    <div id="phoneHelp" class="form-text">Debe ser solo numeros o guines y debe tener como maximo 12 digitos. Ej.</div>
                                                 </div>
                                             </div>
                                             <div className="row mt-4">
